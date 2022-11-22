43b6cad336ff0498aec18f5bb6cb4af60f866aad
henry.shih <siezlh4405@outlook.com>
2022-11-22 23:05:58 +0800

    feat: 完成首頁

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './components/intro/intro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IntroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
